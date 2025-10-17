35e515b13afb363f15ec1c35b1e58f4659bc8489
Drew <103056414+arbowl@users.noreply.github.com>
2025-10-17 18:26:13 -0400

    Fix import

--- frontend/src/pages/VerifyEmailPage.tsx
+++ frontend/src/pages/VerifyEmailPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
-import { api } from '../services/api'
+import api from '../services/api'
 
 const VerifyEmailPage: React.FC = () => {
   const [searchParams] = useSearchParams()
