7803e0c5888ff8b28da8bd7b04b844d1c4526845
Drew <103056414+arbowl@users.noreply.github.com>
2025-10-16 21:14:01 -0400

    Add missing deps

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      buffer: 'buffer/'
+    }
+  },
   server: {
     port: 5173,
     host: true,
@@ -30,6 +35,15 @@ export default defineConfig({
     }
   },
   define: {
-    'process.env': {}
+    'process.env': {},
+    global: 'globalThis'
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      }
+    }
   }
 })
+
