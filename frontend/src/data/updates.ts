474a3ccb82e9b0d28c3e7b8c1471361fa8ce8320
Drew <103056414+arbowl@users.noreply.github.com>
2025-11-22 12:27:52 -0500

    Add report-out update

--- frontend/src/data/updates.ts
+++ frontend/src/data/updates.ts
@@ -11,12 +11,13 @@ export const updates: Update[] = [
     id: 1,
     date: '2025-11-21',
     title: 'Updates',
-    content: `This is where I'll post status updates, site-down alerts, and relevant comments about the project.
+    content: `This is where I'll post status updates, site-down alerts, and relevant musings about the project.
 
 You can expect updates about:
 - System status and maintenance
 - New features and improvements
-- Known issues and active troubleshooting efforts
+- Interesting findings from the data
+- Thoughts on legislative transparency
 
 Stay tuned!`,
     type: 'update'
@@ -35,11 +36,16 @@ I'm working on a smarter system which makes a few improvements to the existing a
 - Reject rows which are procedurally impossible or illogical.
 - Recalculate the notice gap based on the latest rescheduling of a hearing.
 
-I am pursuing these improvements out of a commitment to transparency and accuracy, but note that this does only affect a small subset of bills, the logic is applied evenly to all committees, and it rarely changes a bill’s overall compliance outcome by itself. And, frankly, if it's difficult for an automated tool to analyze a bill, it's not much easier for citizens, which is the exact problem this project aims to shine a spotlight on.
-
-You can stay up to date with the latest changes by visitng the [About](https://beaconhilltracker.org/about) page and scrolling down to the changelog`,
+I am pursuing these improvements out of a commitment to transparency and accuracy, but note that this does only affect a small subset of bills, the logic is applied evenly to all committees, and it rarely changes a bill’s overall compliance outcome by itself.. And, frankly, if it's difficult for an automated tool to analyze a bill, it's not much easier for citizens, which is the exact problem this project aims to shine a spotlight on.`,
     type: 'alert'
-  }
+  },
+  {
+    id: 3,
+    date: '2025-11-22',
+    title: 'Update on Report-Outs and Hearing Announcements',
+    content: `I worked on the reported-out and hearing announcement detection algorithms, ran tests, and landed on a system I'm happier with for today's report. In short, the old system evaluated compliance partially based on what it discovered had happened, rather than adhering solely to what was actually announced. While this technically aligned real-life events as they transpired, it was not a good marker of the story the bill pages told, and could result in situations where the final compliance calculation didn't match what you would actually see in the public record.
+    
+This is the most brittle part of the algorithm due to the constraints, so I'll be on the lookout for further tweaks that need to be made, and will communicate any changes here and in the [changelog](https://beaconhilltracker.org/about). As always, I encourage users to confirm their findings on a bill-by-bill basis using the official Legislature website. The dashboard makes a best-effort attempt to aggregate relevant documentation and grade all committees fairly according to a common ruleset, but it doesn't replace primary sources.`,
+  type: 'update',
+  },
 ]
-
-
